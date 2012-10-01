7b37bafa1800a2f77db12e5660be8c37182a2c31
Luis Velasco <=>
2012-09-30 22:02:55 -0500

    initial commit mmatch_api

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MmatchApi::Application.config.secret_token = '981a0d9dc9f590fd13ab4e3c9b0c10c24c434e6580715ded87428e9a7a0aa230d0afdadf1fc1c2f75feef97df94308d02e7fc3f5f5e1a5cbed8b737123bc3721'
